2cf57f8ffc52d7151515bb585303497351fc6a49
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2021-11-17 19:48:18 +0700

    update apikey x-team

--- src/config.js
+++ src/config.js
@@ -22,7 +22,7 @@ global.APIKeys = { // APIKey nya disini
   'https://hardianto-chan.herokuapp.com': 'hardianto',
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'apikeymu',
+  'https://api.xteam.xyz': 'af527965711d015d',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.xyz': 'tritoapriansyah',
 }
